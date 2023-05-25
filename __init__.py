523a87ee2cd846703c21f8be93a4f649accb0953
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2023-05-24 18:51:23 -0600

    endpoints setted

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from app import App
-
-myapp = App()
-
-if (__name__ == '__main__'):
-    print('a')
-    myapp.createApp()
--- main.py
+++ main.py
@@ -1,18 +1,64 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/api/test', methods=['POST'])
-def test():
+@app.route('/api/classification/freq', methods=['POST'])
+def clalssFreq():
     if (request.is_json):
         content = request.json
-        print(content)
-        return jsonify({"Choo Choo": content}), 200
+
+        nombre = content.get('freName', '')
+        desc = content.get('freDescription', '')
+
+        if (nombre == ''):
+            return jsonify({'message': 'El nombre es obligatorio'}), 400
+        if (desc == ''):
+            return jsonify({'message': 'La descripcion es obligatoria'}), 400
+
+        # aqui ya trabajas tu
+#
+#
+##
+#
+#
+#
+        classification = 'dia'
+
+        return jsonify({"message": 'Clasificacion obtenida correctamente', 'classification': classification}), 200
+
+    return jsonify({'message': 'Peticion no valida'}), 400
+
+
+@app.route('/api/classification/dia', methods=['POST'])
+def classDia():
+    if (request.is_json):
+        content = request.json
+
+        nombre = content.get('diaName', '')
+        desc = content.get('diaDesc', '')
+
+        if (nombre == ''):
+            return jsonify({'message': 'El nombre es obligatorio'}), 400
+        if (desc == ''):
+            return jsonify({'message': 'La descripcion es obligatoria'}), 400
+
+        # aqui ya trabajas tu
+#
+#
+##
+#
+#
+#
+        classification = 'dia'
+
+        return jsonify({"message": 'Clasificacion obtenida correctamente', 'classification': classification}), 200
 
     return jsonify({'message': 'Peticion no valida'}), 400
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
