d22a65f087d789757441131c0cbd6764a1107fe0
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2023-06-02 00:35:36 -0600

    requirements fixed

--- src/prediction.py
+++ src/prediction.py
@@ -1,14 +1,14 @@
 import numpy as pd
 import pandas as pd
 import random
-import matplotlib.pyplot as plt
 import xgboost as xgb
-plt.style.use('fivethirtyeight')
+
 
 def create_features(df):
     df = df.copy()
     df['index'] = range(1, len(df) + 1)
     return df
+
 
 def predictionExpense(gastos):
     minValue = min(gastos)
@@ -31,11 +31,11 @@ def predictionExpense(gastos):
     y_train = df[TARGET]
 
     reg = xgb.XGBRegressor(base_score=0.5, booster='gbtree',
-                            n_estimators=1000,
-                            early_stopping_rounds=50,
-                            objective='reg:squarederror',
-                            max_depth=3,
-                            learning_rate=0.01)
+                           n_estimators=1000,
+                           early_stopping_rounds=50,
+                           objective='reg:squarederror',
+                           max_depth=3,
+                           learning_rate=0.01)
     reg.fit(X_train, y_train,
             eval_set=[(X_train, y_train)],
             verbose=100)
